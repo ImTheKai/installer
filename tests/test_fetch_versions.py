777d45963becc2d41b850e73c05f532e83b5474e
EvgeniyPatlan <evgeniy.patlan@percona.com>
2024-11-21 00:02:42 +0200

    remove old tests

--- tests/test_fetch_versions.py
+++
@@ -1,9 +0,0 @@
-
-import unittest
-from fetch_versions import fetch_latest_versions
-
-class TestFetchVersions(unittest.TestCase):
-    def test_fetch_versions(self):
-        versions = fetch_latest_versions()
-        self.assertIsInstance(versions, dict)
-        self.assertIn("Percona Server for MySQL", versions)
